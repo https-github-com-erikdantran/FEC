a8d7ad11a4c843cb6b8bfcfcb1cdfb6ce6b5d292
Taite Jernigan <taite5100@gmail.com>
2021-10-27 15:56:50 -0700

    Added a review section to the page (barebones)

---
+++ client/src/components/Reviews/Reviews.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        THIS IS A REVIEW
+      </div>
+    )
+  }
+}
+
+export default Reviews
