e4f8966ecbdf638aba403e62e1ab0ed20ddfc740
Taite Jernigan <taite5100@gmail.com>
2021-10-27 12:11:35 -0700

    Added basic framework for our view

---
+++ client/src/components/Product-Overview/ProductPage.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+
+class ProductPage extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      productInfo: null
+    }
+
+    this.getProductInfo = this.getProductInfo.bind(this);
+  }
+
+  componentDidMount() {
+    // place your on load requests here
+    this.getProductInfo(this.props.id);
+  }
+
+  getProductInfo(id) {
+    axios.get(`/api/products/${id}`)
+      .then(results => {
+        this.setState({
+          productInfo: results.data
+        })
+      })
+
+  }
+
+  render() {
+    return (
+      <div>
+        hello World
+        {/* Main Product Info */}
+
+
+        {/* Related Products */}
+
+
+        {/* Q&A */}
+
+
+        {/* Reviews */}
+
+
+      </div>
+    )
+  }
+}
+
+export default ProductPage;
