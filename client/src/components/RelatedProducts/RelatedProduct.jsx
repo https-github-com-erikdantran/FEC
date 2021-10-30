19720d4467042d5732c5754aaf060acbf0ce6003
Erik Dan Tran <erikdantran99@gmail.com>
2021-10-30 14:59:17 -0500

    Added ratings to related products and some styling but will still need to edit that

--- client/src/components/RelatedProducts/RelatedProduct.jsx
+++ client/src/components/RelatedProducts/RelatedProduct.jsx
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 const RelatedProduct = (props) => {
+
+  let getRating = function (ratings) {
+    let totalScore = 0;
+    let numOfScores = 0;
+    for (let key in ratings) {
+      numOfScores += ratings[key] * 1;
+      totalScore += ratings[key] * key;
+    }
+    let rating = Math.round(10 * totalScore / numOfScores) / 10;
+    let percentRating = (rating / 5) * 100 + '%'
+    return { rating, percentRating }
+  }
+
+
+  let { rating, percentRating } = getRating(props.info.ratings)
+
   return (
     <div>
       <p>{props.info.category}</p>
       <div>{props.info.name}</div>
       <p>${props.info.default_price}</p>
-      <p>Reviews here</p>
-      {/* <div><img src={props.info.thumbnail_url}/></div> */}
+      <div className="stars">
+        <div className="percent" style={{ width: percentRating }}></div>
+      </div>
     </div>
   )
 }
