e54002a5bca1eeaab63b5edd548169c99d1e47a1
Taite Jernigan <taite5100@gmail.com>
2021-11-01 20:49:06 -0700

    Rendered some more information to each individual review

--- client/src/components/Reviews/Review.jsx
+++ client/src/components/Reviews/Review.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 function Review(props) {
 
-
+  let percentRating = (rating) => {
+    return Math.round((rating / 5) * 10) * 10 + '%';
+  }
+  console.log(props.review);
   return (
     <div className='review'>
-      {props.review.summary}
+      <div className="stars" style={{fontSize: '11pt'}}>
+          <div className="percent" style={{width: percentRating(props.review.rating)}}>
+          </div>
+        </div>
+      <h4>{props.review.summary}</h4>
+      <p>{props.review.body}</p>
+      {props.review.recommend ?
+      <span><em>&#10003; I recommend this product! </em></span> :
+      null
+      }
     </div>
   )
 }
