9f6b3cb5d1f3b63ec2545a0a285f94b39b48d6c8
Daniel Kim <d95kim@gmail.com>
2021-11-09 09:15:37 -0800

    added thumbnails and fixed image gallery default view

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx'
 import { SocialIcon } from 'react-social-icons';
 
+
 ReactDOM.render(<SocialIcon url="" />, document.getElementById('app'));
 ReactDOM.render(<App />, document.getElementById('app'))
