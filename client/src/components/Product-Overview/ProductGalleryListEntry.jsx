1984504c9c148b9a2ee701b245b26e14dbafbfc3
Daniel Kim <d95kim@gmail.com>
2021-11-10 10:59:20 -0800

    working on select style css styling

--- client/src/components/Product-Overview/ProductGalleryListEntry.jsx
+++ client/src/components/Product-Overview/ProductGalleryListEntry.jsx
@@ -17,8 +17,8 @@ class ProductGalleryListEntry extends React.Component {
     return (
       <div>
         <div onClick={(e) => this.props.handleClickName(e, this.props.style.photos, this.props.style.original_price, this.props.style.sale_price, this.props.style.skus, this.props.style.name)}>
-          <div>
-            {this.props.style.name}
+          <div className="circle">
+            <p>{this.props.style.name}</p>
           </div>
         </div>
       </div>
